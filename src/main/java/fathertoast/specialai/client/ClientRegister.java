501460b3c160dc999def46d27dd27e56a5896cf1
FatherToast <father.toast@hotmail.com>
2023-06-13 19:20:30 -0500

    mod menu button

---
+++ src/main/java/fathertoast/specialai/client/ClientRegister.java
@@ -0,0 +1,19 @@
+package fathertoast.specialai.client;
+
+import fathertoast.crust.api.config.client.ClientConfigUtil;
+import fathertoast.crust.client.KeyBindingEvents;
+import fathertoast.specialai.SpecialAI;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber( value = Dist.CLIENT, modid = SpecialAI.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD )
+public class ClientRegister {
+    /** Called after common setup to perform client-side-only setup. */
+    @SubscribeEvent
+    static void onClientSetup( FMLClientSetupEvent event ) {
+        // Tell Forge to open the config editor when our mod's "Config" button is clicked in the Mods screen
+        ClientConfigUtil.registerConfigButtonAsEditScreen();
+    }
+}
---
+++ src/main/java/fathertoast/specialai/client/package-info.java
@@ -0,0 +1,9 @@
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+@OnClient
+package fathertoast.specialai.client;
+
+import fathertoast.crust.common.util.annotations.OnClient;
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
