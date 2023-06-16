35de34385ba85d8d4abab128f01bfca6f115f5be
FatherToast <father.toast@hotmail.com>
2023-06-16 10:56:45 -0500

    changelog + feature list

--- src/main/java/fathertoast/specialai/SpecialAI.java
+++ src/main/java/fathertoast/specialai/SpecialAI.java
@@ -9,57 +9,88 @@ import org.apache.logging.log4j.Logger;
 import javax.annotation.Nullable;
 
 /**
- * The core of the mod. Contains basic info about the mod, initializes configs, and hooks into FML.
+ * The core of the mod. Contains basic info about the mod and hooks into FML.
  */
 @Mod( SpecialAI.MOD_ID )
 public class SpecialAI {
     
     /* Feature List:
      * (KEY: - = complete in current version, o = incomplete feature from previous version,
-     *       + = incomplete new feature, ? = feature to consider adding)
-     *  - TODO
-     *      - fill this out
-     */
-    
-    /* TODO LIST:
-     *  - Re-implement features:
-     *      + Villages
+     *   + = incomplete new feature, ? = feature to consider adding)
      *
-     * Primary features:
-     *  - New Elite AIs:
-     *      + slam: wields an axe, performs knockback slam "explosion" when damaged in close range
-     *      + fishing: wields a fishing rod and uses it against its target
-     *      + grappling: similar to fishing, but pulls the mob to target instead, mob may mount the target?
-     *  - Reactions:
-     *      + better response to being sniped
-     *  - Door Breaking:
-     *      + fix pathfinding to path through valid doors - is this possible without a mixin on WalkNodeProcessor?
-     *  - Use Item Attack: Replace mob attack AI with "item use", which simulates item use for its "attack"
-     *  - Dig: break down walls when unable to path to target
-     *  - Stalk: try to hide behind blocks when target is looking in their general direction
-     *  - Portals: walk into nearby portals (part of idle fiddling?)
-     *  - Counter-potions: reactive potion drinking (similar to witches) from a small "inventory"
+     * GENERAL
+     *  - animals
+     *      - depacify list (fight back when attacked)
+     *      - aggressive list
+     *      - idle eat breeding items
+     *      ? fix squid not pathing at all and fish moving super slowly in xz-plane
+     *  - jockeys
+     *  - door breaking
+     *      + fix pathfinding to path through all valid doors - is this possible without a mixin on WalkNodeProcessor?
+     *  + dig (break blocks to reach target)
+     *      ? seems to require complete custom pathfinder
+     *  ? use item for attack
+     *      + replace attack ai with "item use", which simulates item use for its "attack"
+     *      ? probably needs flexible config (list of items to use, then one category auto-generated per listed item)
+     *  ? flock (mobs hunt in packs)
+     *  ? spread fire (ignite flammable blocks when on fire and chasing a target)
+     *
+     * REACTIONS
+     *  - avoid explosions
+     *  - call for help (on death)
+     *  - dodge arrows
+     *  + aggro when sniped (enable revenge targeting beyond follow range)
+     *  ? reactive potions (potion drinking from a small "inventory" - similar to witch logic)
+     *      ? how the heck to animate this?
+     *      ? chance to drop potions
      *      + health (damage for undead) when low on health
-     *	    + fire resistance when on fire or taking fire damage
-     *	    + water breathing when drowning
-     *	    ? strength when target is armored/enchanted
-     *	    ? invisibility when shot by arrows
-     *  - Bosses: Rare, powerful monsters
-     *      + Nemesis system? (See Darkmega)
-     *      + Name influenced by entity type
-     *      + Uses a "unique" item that is a guaranteed drop
-     *      + Unique item is enchanted and has a special enchantment/modifier
-     *      + The special modifier is more powerful than otherwise obtainable, but may come with a drawback
-     *      + Unique item is named based on its special (prefixed by "<boss_name>'s")
-     *      + Uses the counter-potion system
-     *	? Flock: hunt together
-     *  ? Spread fire: ignite flammable blocks when on fire and chasing a target
+     *      + fire resistance when on fire or taking fire damage
+     *      + water breathing when drowning
+     *      ? strength when target is armored/enchanted
+     *      ? speed when target is far away
+     *      ? invisibility when shot by arrows
+     *  ? stalk (try to hide when target is looking)
      *
-     * Utility features:
-     *  - Fix squid not pathing at all and fish moving super slowly in xz-plane
-     *  - More editing for AI, particularly for editing pre-existing AI
-     *  - An extra whitelist for each elite AI to apply it directly
-     *  - More effective configs, more per-entity-id options
+     * IDLE BEHAVIORS
+     *  - griefing (break blocks)
+     *  - fiddling (right-click blocks)
+     *  ? walk into portals
+     *
+     * ELITE AI
+     *  ? extra whitelist for each elite ai to apply it directly
+     *  - leap
+     *  - jump
+     *  - sprint
+     *  - barrage
+     *  - charge
+     *  - thief
+     *  - shaman
+     *  - spawner
+     *  - throw ally
+     *  - throw enemy
+     *  + slam (wields an axe, performs knockback slam "explosion" when damaged in close range)
+     *  + fishing (wields a fishing rod and uses it against its target)
+     *  + grappling (similar to fishing, but pulls the mob to target instead, mob may mount the target?)
+     *
+     * VILLAGES - TODO all these features must be re-evaluated due to village overhaul
+     *  o rep is never forgotten (vanilla bug fix)
+     *  o hostile against very low rep players
+     *  o rep affected by building or breaking certain blocks in village
+     *  o chance to attack when breaking blocks in village
+     *  o rep gained by killing hostile mobs near village
+     *  o command to check your village rep
+     *
+     * BOSSES
+     *  + name influenced by entity type and abilities
+     *  + "unique" item
+     *      + equipped, guaranteed drop
+     *      + has a special enchantment/modifier (may be otherwise unobtainable, may have drawback)
+     *      + named based on boss and special (prefixed by "<boss_name>'s")
+     *  ? Nemesis system
+     *
+     * AI EDITOR
+     *  ? enable complete (as possible) editing of pre-existing ai
+     *  ? is this too much to include with this mod? maybe have the whole system only enabled by a config option
      */
     
     /** The mod id and namespace used by this mod. */
